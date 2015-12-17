77008f97c47c06e008c79155dae975f873896d95
adeq123 <marek.czwartek@wp.pl>
2015-12-17 20:07:28 +0100

    Initial Commit

---
+++ AGH1/src/myPackage/HelloWorld.java
@@ -0,0 +1,17 @@
+package myPackage;
+/**
+ * Klasa wyœwietlajaca powitaniena konsoli.
+ * @author Szymon
+ */
+public class HelloWorld {
+    /**
+     *
+     * @param argv lista argumentów przekazywana do programu
+     */
+   public static void main(String [] argv){
+       System.out.println("Hello World!");
+  }
+}
+
+
+
